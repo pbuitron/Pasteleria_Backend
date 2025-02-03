0b24b1efc4f0528027d46633ab095b64db46faeb
pbuitron <pbuitron@users.noreply.github.com>
2025-02-03 08:57:57 -0500

    COnfiguración de .env para vercel

--- src/config/database.js
+++ src/config/database.js
@@ -3,17 +3,10 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-
-const dbUser  = process.env.DB_USER;
-const dbPassword = process.env.DB_PASSWORD;
-const dbHost = process.env.DB_HOST;
-const dbName = process.env.DB_NAME;
-
-
 const connectToDDBB = async () => {
 
   try {
-    const connectDDBB = mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@${dbHost}/${dbName}?retryWrites=true&w=majority&appName=Backend`)
+    const connectDDBB = mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Backend`)
     console.log(`Mongo Connected:${(await connectDDBB).connection.host} `);
   } catch (error) {
     console.error('Error de conexión MongoDB:', error);
